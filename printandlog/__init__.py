fce44b67e560b34a17c5a0ecf0fc3fdd36e474e7
Tibibv <kron_bv@yahoo.com>
2017-12-13 17:29:36 +0200

    Replace printandlog.py with definition.py

--- printandlog/__init__.py
+++ printandlog/__init__.py
@@ -1,1 +1,2 @@
-import printandlog
+import definition
+
--- printandlog/tests/printandlogtest.py
+++ printandlog/tests/printandlogtest.py
@@ -1,21 +1,21 @@
 from unittest import TestCase
-import printandlog
+import definition
 import re
 import os
 
 class Test_printandlogtest(TestCase):
   def test_file_name(self):
-    filePath = printandlog.makeUniqueFileName("CompareFolders.txt")
+    filePath = definition.makeUniqueFileName("CompareFolders.txt")
     dirName = os.path.dirname(filePath)
     fileName = os.path.basename(filePath)
-    self.assertEquals(dirName, printandlog.defaultFolder())
+    self.assertEquals(dirName, definition.defaultFolder())
     reFileName = re.compile('CompareFolders_\d\d\d\d_\d\d_\d\d_\d\d_\d\d_\d\d\.txt')
     self.assertIsNotNone(reFileName.match(fileName))
 
   def test_file_path(self):
     initialFilePath = "D:\Log\Log\CompareFolders.txt"
     initialDirName = os.path.dirname(initialFilePath)
-    filePath = printandlog.makeUniqueFileName(initialFilePath)
+    filePath = definition.makeUniqueFileName(initialFilePath)
     dirName = os.path.dirname(filePath)
     fileName = os.path.basename(filePath)
     self.assertEquals(dirName, initialDirName)
